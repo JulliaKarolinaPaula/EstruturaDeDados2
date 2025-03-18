309877e6594dd5a9313ef5a39d3d9a61db4ba270
Jullia Karolina <135389065+JulliaKarolinaPaula@users.noreply.github.com>
2025-03-18 13:50:45 -0300

    Add files via upload

---
+++ bubbleSort.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int *lista, int n){
+    int aux;
+      for(int i = 0;i < (n -1);i++){
+         for(int j = 0;j < (n - i - 1);j++){
+            if(lista[j] > lista[j+1]){
+                  aux = lista[j+1];
+                  lista[j+1] = lista[j];
+                  lista[j] = aux;
+            }
+         }
+      }
+};
+
+int main(){
+      int n;
+
+      scanf("%d", &n);
+
+      int lista[n];
+
+      for(int i = 0;i < n;i++){
+            scanf("%d", &lista[i]);
+      }
+
+      printf("\nLISTA DESORDENADA: ");
+      for(int i = 0;i < n;i++){
+           printf("%d ", lista[i]);
+      }
+      
+      bubbleSort(&lista[0], n);
+
+      printf("\nLISTA ORDENADA: ");
+      for(int i = 0;i < n;i++){
+            printf("%d ", lista[i]);
+      }
+
+
+
+      return 0;
+}
